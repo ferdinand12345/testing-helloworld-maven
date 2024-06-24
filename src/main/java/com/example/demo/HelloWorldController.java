523054f803342820313054a176714f52836543b6
administrator <ppt_arp@bri.co.id>
2024-06-24 12:28:14 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	// GET HTTP Method
+	// http://localhost:8080/hello-world
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
